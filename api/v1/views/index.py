c3397df47c617b56e60214216d6cb9a3283b85c4
sagebeme <eudicas5k@gmail.com>
2023-04-29 19:18:30 +0300

    retrieves all obj in database and converts them to JSON

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,13 +3,33 @@
 """
 Index file
 """
-
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.user import User
+from models import storage
 from api.v1.views import app_views
 from flask import jsonify
 
-@app_views.route('/status')
+
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """
     status: Status of API
     """
     return jsonify({"status":"OK"})
+
+
+@app_views.route('/stats', methods=['GET'], strict_slashes=False)
+def number_obj():
+    """
+    endpoint: gets number of each objects by type
+    """
+    classes = [Amenity, City, Place, Review, State, User]
+    names = ["amenity", "city", "place", "review", "states", "users"]
+
+    num_obj = {}
+    for i in range(len(classes)):
+        num_obj[names[i]] = storage.count(classes[i])
+    return jsonify(num_obj)
