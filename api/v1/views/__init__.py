4841d5c64737bf4d75c376c328847e0f22c2ba7d
sagebeme <eudicas5k@gmail.com>
2023-05-01 12:22:09 +0300

    adding status path

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -2,14 +2,17 @@
 """
     Blueprint for API
 """
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
-app_views = Blueprint('app_views',
-                      __name__,
-                      url_prefix='/api/v1')
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
 from api.v1.views.places_reviews import *
 from api.v1.views.amenities import *
+
+
+@app_views.route('/status')
+def status():
+    return jsonify({"status":"OK"})
