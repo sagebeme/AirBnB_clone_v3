1a4a47f0893f1901d86702db2bfe5f001bd938a6
sagebeme <eudicas5k@gmail.com>
2023-05-01 12:27:24 +0300

    removing and adding the status and registering the blueprint

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,7 @@ from flask import Flask, jsonify
 
 
 app = Flask(__name__)
-app.register_blueprint(app_views)
+app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
 @app.teardown_appcontext
